32bb8b29f92ebce71b5d72893f96d95befd2b99a
n01r1r_ <dyhan724@gmail.com>
2025-06-03 13:49:58 +0900

    fix error in getPosts.ts for fetching notion page

--- src/apis/notion-client/getPosts.ts
+++ src/apis/notion-client/getPosts.ts
@@ -12,7 +12,8 @@ import { TPosts } from "src/types"
 
 // TODO: react query를 사용해서 처음 불러온 뒤로는 해당데이터만 사용하도록 수정
 export const getPosts = async () => {
-  let id = CONFIG.notionConfig.pageId as string
+  //let id = CONFIG.notionConfig.pageId as string
+  let id = CONFIG.notionConfig.page as string
   const api = new NotionAPI()
 
   const response = await api.getPage(id)
