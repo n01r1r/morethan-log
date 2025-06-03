d6cd52c89303d7412b610342a4e2b986c8a0a748
n01r1r_ <dyhan724@gmail.com>
2025-06-03 17:53:10 +0900

    Removed service from the main page

--- site.config.js
+++ site.config.js
@@ -10,12 +10,14 @@ const CONFIG = {
     github: "n01r1r",
     instagram: "",
   },
+  /*
   projects: [
     {
       name: `morethan-log`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
+  */
   // blog setting (required)
   blog: {
     title: "n01r-blog",
