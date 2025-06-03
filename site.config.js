1b1e58997d6029ece79791e2f55f72f37d299967
n01r1r_ <dyhan724@gmail.com>
2025-06-03 17:51:35 +0900

    Edited profile images and misc.

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Dongyeob Han",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "Student",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "dyhan724@gmail.com",
+    linkedin: "dyhan",
+    github: "n01r1r",
     instagram: "",
   },
   projects: [
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "n01r-blog",
+    description: "n01r's blog",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
